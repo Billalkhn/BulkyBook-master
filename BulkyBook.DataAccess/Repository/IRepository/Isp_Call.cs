71c3813d243fd3d7671c517ea0f42c95242be83d
rohmazahid98@gmail.com <rohmazahid98@gmail.com>
2020-06-22 00:21:03 +0500

    Product module complete Compnay

---
+++ BulkyBook.DataAccess/Repository/IRepository/Isp_Call.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface Isp_call : IDisposable
+    {
+        //StroredProcedure Calls
+        //using Dapper for passing all parameters...
+        //Bilal Khan
+        T Single<T>(string procedureName, DynamicParameters param = null);//Single Value return (like count , int ot bool value)
+        void Execute(string procedureName, DynamicParameters param = null);//create,remove,updating for exuting into the database
+
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);//get complete row one record of any item 
+
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);// Get allthe rows or data 
+
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);//get two table
+            
+     }
+}
